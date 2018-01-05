724281edfdbdab5b3b1ad2f1aaecd6787ef11d53
Brandon Lew <brandon.t.lew@gmail.com>
2018-01-05 11:01:19 -0800

    trim data, add data conversion, streamline data requests

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-// CREATE BUTTONS FOR ALL COUNTRIES SORTED BY COMMON NAME
 var countryName = [];
 var userMap = [];
 $.ajax({
   url: 'data/countries.json',
   dataType: 'json',
   success: function(data) {
+    // CREATE BUTTONS FOR ALL COUNTRIES SORTED BY NAME
     for(var i = 0; i < data.length; i++) {
-      countryName.push(data[i].name.common);
+      countryName.push(data[i].name);
     }
     countryName.sort();
     for(var v = 0; v < countryName.length; v++) {
@@ -20,14 +20,13 @@ $.ajax({
     }
     // GET SELECTED COUNTRIES
     function selected() {
-      userMap = [];
       var checkboxes = document.getElementsByTagName('input');
       for(var x = 0; x < checkboxes.length; x++) {
         if(checkboxes[x].checked) {
           for(var i = 0; i < data.length; i++) {
-            if(checkboxes[x].value === data[i].name.common) {
-              var allData = {"name":data[i].name.common,"latlng":[data[i].latlng[0],data[i].latlng[1]]};
-              userMap.push(allData);
+            if(checkboxes[x].value === data[i].name) {
+              var userLocations = data[i].name;
+              userMap.push(userLocations);
             }
           }
         }
--- js/usermap.js
+++ js/usermap.js
@@ -1,28 +1,37 @@
 var google;
 var locationCoords = [];
-var locationName = [];
 // GET USER LOCATION DATA
 $.ajax({
   url: "locations.json",
   dataType: "text",
-  success: function(data) {
-    // PARSE AND PUSH DATA TO ARRAYS
-    var locations = $.parseJSON(data);
-    for(var x = 0; x < locations.length; x++) {
-      var countryCoords = {lat: locations[x].latlng[0], lng: locations[x].latlng[1]};
-      var countryName = locations[x].name;
-      locationCoords.push(countryCoords);
-      locationName.push(countryName);
-    }
-    // PRINT LIST OF VISITED COUNTRIES BELOW THE MAP
-    for(var z = 0; z < locationName.length; z++) {
-      var visited = '<li>' + locationName[z] + '</li>';
-      document.getElementById('visitedList').innerHTML += visited;
-    }
-    // PRINT NUMBER AND PERCENT OF COUNTRIES VISITED
-    document.getElementById('congrats').innerHTML = '<p>Congrats! You have visited ' + locationName.length + ' countries/territories.</p><p>' + Math.round(locationName.length/247*100) + '% of 247 countries/territories around the globe.</p>'
-    // INITIALZE MAP WITH USER LOCATION DATA
-    initMap();
+  success: function(userData) {
+    // PARSE USER LOCATION JSON
+    var userLocations = $.parseJSON(userData);
+    // GET COUNTRY DATA
+    $.ajax({
+      url: "/portfolio/traveltracker/data/countries.json",
+      dataType: "text",
+      success: function(countryData) {
+        // PARSE COUNTRY DATA JSON
+        var countryLocations = $.parseJSON(countryData);
+        for(var x = 0; x < userLocations.length; x++) {
+          // PRINT LIST OF VISITED COUNTRIES BELOW THE MAP
+          var visited = '<li>' + userLocations[x] + '</li>';
+          document.getElementById('visitedList').innerHTML += visited;
+          // PRINT NUMBER AND PERCENT OF COUNTRIES VISITED
+          document.getElementById('congrats').innerHTML = '<p>Congrats! You have visited ' + userLocations.length + ' countries/territories.</p><p>' + Math.round(userLocations.length/247*100) + '% of 247 countries/territories around the globe.</p>'
+          // CREATE ARRAY OF LOCATION COORDS FOR GOOGLE MAPS
+          for(var i = 0; i < countryLocations.length; i++) {
+            if(userLocations[x] === countryLocations[i].name) {
+              var countryCoords = {lat: parseFloat(countryLocations[i].latlng[0]), lng: parseFloat(countryLocations[i].latlng[1])};
+              locationCoords.push(countryCoords);
+            }
+          }
+          // INITIALZE MAP WITH USER LOCATION DATA
+          initMap();
+        }
+      }
+    })
   }
 });
 
